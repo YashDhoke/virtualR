7c37ed1ad9235b12490dc859a3e59f96bd852b97
YashDhoke <yashdhoke1@gmail.com>
2025-03-09 23:36:19 +0530

    text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
           className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
         >
           <source src={video2} type="video/mp4" />
-          Your Browser does not support the video tag
+          Your Browser does not support the video tag .
         </video>
       </div>
     </div>
