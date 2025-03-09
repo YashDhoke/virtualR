bde109a7c7a5098c5360ee70ad761b7b013d1c2c
Yash Dhoke <yashdhoke@Yashs-MacBook-Air.local>
2025-03-09 23:30:33 +0530

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <div className="max-w-7xl mx-auto p-20 px-6">
+        <HeroSection />
+      </div>
     </div>
   );
 };
